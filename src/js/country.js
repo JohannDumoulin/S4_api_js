db9bc318aae801c3ea84b52d02613e6fef572f82
thejojo974 <johanndumoulin734@gmail.com>
2020-04-02 22:16:36 +0200

    Update Inté + Dev

--- src/js/country.js
+++ src/js/country.js
@@ -21,7 +21,12 @@ export default class Country {
 	initEls () {
 		this.$els = {
 			country: $('.js_country_name'),
-			capital: $('.js_country_capital')
+			capital: $('.js_capital'),
+			flag: $('.js_flag_container'),
+			population: $('.js_population'),
+			region: $('.js_region'),
+			currency: $('.js_currency'),
+			language: $('.js_language')
 		}
 	}
 	initEvents(country) {
@@ -38,16 +43,29 @@ export default class Country {
 		$.getJSON(api.endpoint, api.params)
 		.then((response) => {
 			console.log(response);
-			this.renderCountry(response[0].name, response[0].capital);
+			this.renderCountry(
+				response[0].name,
+				response[0].capital,
+				response[0].flag,
+				response[0].population,
+				response[0].region,
+				response[0].currencies[0].name,
+				response[0].languages[0].name
+			);
 		})
 		.catch((e) => {
 			console.log('error with the quote:', e)
 		});
 	}
 
-	renderCountry(country, capital) {
+	renderCountry(country, capital, flag, population, region, currency, language) {
 		this.$els.country.text(country);
-		this.$els.capital.text(capital);
+		this.$els.capital.prepend(`<p>${capital}</p>`);
 		new Background(capital);
+		this.$els.flag.prepend(`<img id="flag" src="${flag}" alt="Flag of ${country}">`);
+		this.$els.population.prepend(`<p>${population}</p>`);
+		this.$els.region.prepend(`<p>${region}</p>`);
+		this.$els.currency.prepend(`<p>${currency}</p>`);
+		this.$els.language.prepend(`<p>${language}</p>`);
 	}
 }
